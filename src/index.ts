75eea628b1aa8d25bd793c7481e106247da7b44e
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-08-29 14:06:41 -0500

    custom map class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 /// <reference types="@types/google.maps" />
 
+import { CustomMap } from './CustomMap';
+
 // import { User } from './User';
 // import { Company } from './Company';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const map = new CustomMap('map');
