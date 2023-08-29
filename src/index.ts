ae38dab81e83e656a61904e786d634d5f997cc9e
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-08-29 12:25:59 -0500

    map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
 
-const user = new User();
-const company = new Company();
+// import { User } from './User';
+// import { Company } from './Company';
 
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
