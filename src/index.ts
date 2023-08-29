3430030bd2b15852cf3821a5141678885b748455
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-08-29 14:13:25 -0500

    add markers with duplication

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 /// <reference types="@types/google.maps" />
 
 import { CustomMap } from './CustomMap';
+import { User } from './User';
+import { Company } from './Company';
 
-// import { User } from './User';
-// import { Company } from './Company';
-
+const user = new User();
+const company = new Company();
 const map = new CustomMap('map');
+
+map.addUserMarker(user);
+map.addCompanyMarker(company);
